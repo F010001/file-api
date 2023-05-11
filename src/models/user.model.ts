186baa3683e6c60b229efe310f214e414caa0440
farid <farid@example.com>
2023-05-11 11:34:50 +0300

    add dep

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,7 +3,9 @@ import { connection } from '../db';
 import { Record } from './record.model';
 
 export const User = connection.define('users', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  id: { type: DataTypes.STRING, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
 });
+
+User.hasMany(Record);
